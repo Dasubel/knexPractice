34faa3e88fdd0a5de8ef12bc98444aa9e313128f
Corey <dasubel@yahoo.com>
2023-03-21 14:57:41 -0600

    initial push

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const port = 8081;
+const knex = require('knex')(require('../knexfile.js')["development"])
 
 app.get('/', (request, response) => {
     response.send('Application up and running.')
+})
+
+app.get('/pets', (req, res) => {
+    knex
+        .select('*')
+        .from('pet')
+        .then(pets => {
+            var petNames = pets.map(pet => pet.name)
+            res.status(200).json(petNames)
+        })
 })
 
 app.listen(port, () => {
